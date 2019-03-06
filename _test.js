9ed6ec8d52ebfc5b29e372ffb8d0032651ac7c98
DaShinja <byron@dashinja.com>
2019-03-05 19:41:34 -0500

    not working | save state

--- _test.js
+++ _test.js
@@ -1,9 +1,17 @@
-const { YouTube } = require("./app");
+const { YouTube, Twitch } = require('./app')
 
-__Test__();
+// __Test__()
+__Test__Twitch()
 
 function __Test__() {
-  YouTube("UCBUVGPsJzc1U8SECMgBaMFw", "YOUR_API_KEY").then(data => {
-    console.log(data);
-  });
+  YouTube('UCBUVGPsJzc1U8SECMgBaMFw', 'YOUR_API_KEY').then(data => {
+    console.log(data)
+  })
+}
+
+async function __Test__Twitch() {
+  const data = await Twitch('lirik', function callback(items) { return items })
+
+  console.log('Twitch Data:')
+  return console.log("Data:",data)
 }
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const YouTube = require('./lib/youtube');
+const YouTube = require('./lib/youtube')
 // const instagram = require('./lib/instagram');
-require('dotenv').config();
+const Twitch = require('./lib/twitchScrape')
+require('dotenv').config()
 
 module.exports = {
-  YouTube: async (channelID, key) => (await YouTube(channelID, key, 5)),
+  YouTube: async (channelID, key) => await YouTube(channelID, key, 5),
   // Instagram: async (instaId) => (await simpleInstagramScraper.getReport(instaId)),
-  }
+
+  Twitch: async (liveChannel, callback) => await Twitch(liveChannel, callback),
+}
